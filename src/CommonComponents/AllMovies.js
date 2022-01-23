845c199f258024332e72c0c5c943d64f222eed9d
Shardul K <shardulkavar80@gmail.com>
2022-01-23 20:01:50 +0530

    cleaned up code a bit for allMovies file with array map for row catagories

--- src/CommonComponents/AllMovies.js
+++ src/CommonComponents/AllMovies.js
@@ -1,67 +1,36 @@
-import React, { useEffect } from "react";
+import React from "react";
 import requesets from "./requests";
 import Row from "./Row";
 
 function AllMovies({ bookmarkThis, removeBookmark, watchlist }) {
+  const rowCatagories = [
+    { title: "NETFLIX ORIGINALS", url: requesets.fetchNetflixOriginals },
+    { title: "Trending Now", url: requesets.fetchTrending },
+    { title: "Top Rated", url: requesets.fetchTopRated },
+    { title: "Action Movies", url: requesets.fetchActionMovies },
+    { title: "Comedy Movies", url: requesets.fetchComedyMovies },
+    { title: "Horror Movies", url: requesets.fetchHorrorMovies },
+    { title: "Romance Movies", url: requesets.fetchRomanceMovies },
+    { title: "Documentaries", url: requesets.fetchDocumentaries },
+  ];
+
   return (
     <div>
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requesets.fetchNetflixOriginals}
-        isLargeRow
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        watchlist={watchlist}
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        fetchUrl={requesets.fetchTrending}
-        watchlist={watchlist}
-        title="Trending Now"
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        fetchUrl={requesets.fetchTrending}
-        watchlist={watchlist}
-        title="Top Rated"
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        watchlist={watchlist}
-        title="Action Movies"
-        fetchUrl={requesets.fetchActionMovies}
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        watchlist={watchlist}
-        title="Comedy Movies"
-        fetchUrl={requesets.fetchComedyMovies}
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        watchlist={watchlist}
-        title="Horror Movies"
-        fetchUrl={requesets.fetchHorrorMovies}
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        watchlist={watchlist}
-        title="Romance Movies"
-        fetchUrl={requesets.fetchRomanceMovies}
-      />
-      <Row
-        bookmarkThis={bookmarkThis}
-        removeBookmark={removeBookmark}
-        watchlist={watchlist}
-        title="Documentaries"
-        fetchUrl={requesets.fetchDocumentaries}
-      />
+      <>
+        {rowCatagories.map((row) => {
+          console.log(row.title === "Trending Now");
+          return (
+            <Row
+              isLargeRow={row.title === "NETFLIX ORIGINALS"}
+              title={row.title}
+              fetchUrl={row.url}
+              bookmarkThis={bookmarkThis}
+              removeBookmark={removeBookmark}
+              watchlist={watchlist}
+            />
+          );
+        })}
+      </>
     </div>
   );
 }
